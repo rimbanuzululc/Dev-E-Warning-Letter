b97410c29b6172bf7eef139fb36ece10b349f0cb
Rimba Chory <nuzululchory@gmail.com>
2021-01-28 22:19:28 +0700

    Revert "kapos"
    This reverts commit 2ea6dfa46f17ff3db2215f8771a63c233627ffcd.

---
+++ foundation/foundationsomasiimpl/src/main/java/com/sli/somasi/foundation/dao/MappingAreaKAPosDAO.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sli.somasi.foundation.dao;
+
+import com.sli.somasi.foundation.dto.MappingAreaKAPos;
+import io.starlight.db.CommonDAO;
+import io.starlight.db.DAO;
+import io.vertx.core.Future;
+import java.util.List;
+
+/**
+ *
+ * @author hp
+ */
+@DAO(config = "db")
+public class MappingAreaKAPosDAO extends CommonDAO {
+    
+    public Future<MappingAreaKAPos> add (MappingAreaKAPos area) {
+        
+        Future<MappingAreaKAPos> result = Future.future();
+        
+        insert(area)
+                .setHandler(ret -> {
+                    
+                    if (ret.succeeded()) {
+                            result.complete(ret.result());
+                    } else {
+                            result.complete(ret.result());
+                    }
+                
+                });
+        return result;
+    }
+    
+    public Future<List<MappingAreaKAPos>> list () {
+        return queryScript("listMappingArea", MappingAreaKAPos.class);
+    }
+    
+}
