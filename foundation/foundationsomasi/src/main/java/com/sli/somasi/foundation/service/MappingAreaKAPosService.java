078c7adbfb0001ed893daf98b6b811432613b7f4
Rimba Chory <nuzululchory@gmail.com>
2021-01-28 22:22:24 +0700

    Revert "Revert "Revert "kapos"""
    This reverts commit 14970d14338774a0d180fcc303a82cd40dd4c16a.

---
+++ foundation/foundationsomasi/src/main/java/com/sli/somasi/foundation/service/MappingAreaKAPosService.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sli.somasi.foundation.service;
+
+import com.sli.somasi.foundation.dto.AgentPos;
+import com.sli.somasi.foundation.dto.District;
+import com.sli.somasi.foundation.dto.MappingAreaKAPos;
+import io.vertx.core.Future;
+import java.util.List;
+
+/**
+ *
+ * @author hp
+ */
+public interface MappingAreaKAPosService {
+    
+    Future<MappingAreaKAPos> add (MappingAreaKAPos area);
+    Future<Boolean> delete (Integer id);
+    Future<List<MappingAreaKAPos>> list();
+    Future<List<District>> listDistByArea (int id);
+}
